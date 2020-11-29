964cf9d69c94c8a7f237db334b5aa4f6a7117965
thanhduydt <0306181301@caothang.edu.vn>
2020-11-29 12:46:32 +0700

    San duy branch

---
+++ Shop/Areas/Admin/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Areas.Admin.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int pid { get; set; }
+        public int aid { get; set; }
+    }
+}
