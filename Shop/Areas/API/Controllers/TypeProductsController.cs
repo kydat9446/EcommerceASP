8d1575453bcc8cf7e48715ca5e5ceb7921ba2df8
Tran Vo Dang Khoa <0306181328@caothang.edu.vn>
2020-12-04 13:48:41 +0700

    API

---
+++ Shop/Areas/API/Controllers/TypeProductsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Areas.Admin.Data;
+using Shop.Areas.Admin.Models;
+
+namespace Shop.Areas.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypeProductsController : ControllerBase
+    {
+        private readonly DPContext _context;
+
+        public TypeProductsController(DPContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TypeProducts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TypeProduct>>> GettypeProduct()
+        {
+            return await _context.typeProduct.ToListAsync();
+        }
+
+        // GET: api/TypeProducts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TypeProduct>> GetTypeProduct(int id)
+        {
+            var typeProduct = await _context.typeProduct.FindAsync(id);
+
+            if (typeProduct == null)
+            {
+                return NotFound();
+            }
+
+            return typeProduct;
+        }
+
+        // PUT: api/TypeProducts/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTypeProduct(int id, TypeProduct typeProduct)
+        {
+            if (id != typeProduct.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(typeProduct).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TypeProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TypeProducts
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<TypeProduct>> PostTypeProduct(TypeProduct typeProduct)
+        {
+            _context.typeProduct.Add(typeProduct);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTypeProduct", new { id = typeProduct.Id }, typeProduct);
+        }
+
+        // DELETE: api/TypeProducts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<TypeProduct>> DeleteTypeProduct(int id)
+        {
+            var typeProduct = await _context.typeProduct.FindAsync(id);
+            if (typeProduct == null)
+            {
+                return NotFound();
+            }
+
+            _context.typeProduct.Remove(typeProduct);
+            await _context.SaveChangesAsync();
+
+            return typeProduct;
+        }
+
+        private bool TypeProductExists(int id)
+        {
+            return _context.typeProduct.Any(e => e.Id == id);
+        }
+    }
+}
